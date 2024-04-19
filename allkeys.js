0868bdc8d4b0f0a64b832596feb35796ecd0c80d
habiben411 <hamid.sarve@gmail.com>
2024-04-20 00:04:30 +0200

    Chapter 2 updates

--- allkeys.js
+++ allkeys.js
@@ -4,17 +4,20 @@ var audio_appl = new Audio("audio/applause.mp3");
 
 // Add an event listener for the keydown event
 window.addEventListener("keydown", function(event) {
- // Check if the 'A' key was pressed
- if (event.key === "d" || event.key === "D") {
-    // Play the sound
-    audio_drums.play();
-    console.log("Playing drum sounds..."); 
- }
- if (event.key === "a" || event.key === "A") {
-    // Play the sound
-    audio_appl.play();
-    console.log("Playing appl sounds..."); 
- }
-
+    // Check if the 'A' key was pressed
+    if (event.key === "d" || event.key === "D") {
+        // Play the sound
+        audio_drums.play();
+        console.log("Playing drum sounds..."); 
+    }
+    if (event.key === "a" || event.key === "A") {
+        // Play the sound
+        audio_appl.play();
+        console.log("Playing appl sounds..."); 
+    }
+    if (event.key === 'm') {
+        // Redirect to another URL
+        window.location.href = '/menu.html';
+    }
 });
 
