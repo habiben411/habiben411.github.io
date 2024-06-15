a947e86d50f2fcc36877a7fcb8638bbcbe30f5c7
habiben411 <hamid.sarve@gmail.com>
2024-06-15 17:09:37 +0200

    Final Fixes Saturday

--- allkeys.js
+++ allkeys.js
@@ -32,38 +32,19 @@ window.addEventListener("keydown", function(event) {
 
 
 window.onload = function() {
-    
+
+  /*
   cur = './images/mycursor.ico'
   pointer = "url('" + cur + "'), auto"
   document.body.style.cursor = pointer;
   console.log("change cursor? " + pointer);
   cur_svg = '.' + cur
   pointer_svg = "url('" + cur_svg + "'), auto"
-/*
-  var svgObjects = document.querySelectorAll('object[type="image/svg+xml"]');
 
-  svgObjects.forEach(function(svgObject) {
-    var svgDoc = svgObject.contentDocument || svgObject.contentWindow.document;
-    console.log("changing cursor for  object " + svgObject);
-    svgObject.style.cursor = pointer_svg;
 
-    // Find all SVG elements within the SVG document
-    var svgElements = svgDoc.querySelectorAll('svg');
-    console.log("elements: " + svgElements);
-
-    svgElements.forEach(function(element) {
-      console.log("changing cursor for SVG " + element);
-      console.log("changing cursor for SVG to " + pointer_svg);
-
-      element.addEventListener('mousemove', function() {
-        this.style.cursor = pointer_svg; // Change cursor to 'pointer' or any other style
-      });
-    });
-  });
-*/
   document.querySelectorAll('a').forEach(link => {
     console.log("changeing link " + link + " to " + pointer);
     link.style.cursor = pointer;
   });
-
+*/
 };
